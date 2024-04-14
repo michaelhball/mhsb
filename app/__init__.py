1565aa45d4a994594c463108391878d79c611396
Michael Ball <michael.h.s.ball@gmail.com>
2024-04-14 15:04:36 +0200

    Adds Flask-Assets to enable scss

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_assets import Bundle, Environment
 from flask_font_awesome import FontAwesome
 
 from app.config import Config
@@ -7,5 +8,11 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 font_awesome = FontAwesome(app)
+
+# Flask-Assets setup to enable SCSS
+assets = Environment(app)
+scss_bundle = Bundle("scss/*.scss", output="styles/compiled.css", filters=["libsass"])
+assets.register("scss_all", scss_bundle)
+scss_bundle.build()
 
 from app import routes  # noqa: E402, F401
--- app/config.py
+++ app/config.py
@@ -3,3 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", default="devilously devious")  # TODO: prod ...
+
+    ASSETS_DEBUG = True
